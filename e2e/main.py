e3ef990d72b0f034bcf8cef6d97753c5526d0ee0
Ringo Hoffmann <r.hoffmann@b12-gruppe.de>
2023-05-08 13:21:35 +0200

    add e2e tests

---
+++ e2e/main.py
@@ -0,0 +1,65 @@
+import akane
+
+
+@akane.test()
+def install_sdk():
+    akane.exec("goup use 1.20.4")
+    res = akane.exec("go version")
+    akane.assert_true(
+        res.startswith("go version go1.20.4"),
+        f"res was {res[:-1]}")
+
+
+@akane.test()
+def install_another_sdk():
+    akane.exec("goup use 1.19.0")
+    res = akane.exec("go version")
+    akane.assert_true(
+        res.startswith("go version go1.19"),
+        f"res was {res[:-1]}")
+
+
+@akane.test()
+def current():
+    res = akane.exec("goup current")
+    akane.assert_eq("1.19\n", res)
+
+
+@akane.test()
+def check_for_update():
+    res = akane.exec("goup check")
+    lines = res.split("\n")
+    akane.assert_eq("New minor version is available!", lines[0][5:])
+    akane.assert_true(lines[1].startswith("1.19"))
+    akane.assert_eq("New patch version is available!", lines[2][5:])
+    akane.assert_true(lines[3].startswith("1.19"))
+
+
+@akane.test()
+def switch_back():
+    akane.exec("goup use 1.20.4")
+    res = akane.exec("go version")
+    akane.assert_true(
+        res.startswith("go version go1.20.4"),
+        f"res was {res[:-1]}")
+
+
+@akane.test()
+def list_local():
+    res = akane.exec("goup ls")
+    exp = "  1.19\n" \
+          "* 1.20.4\n"
+    akane.assert_eq(exp, res)
+
+
+@akane.test()
+def cleanup():
+    akane.exec("goup clean --yes")
+
+
+def main() -> int:
+    return akane.run_all()
+
+
+if __name__ == "__main__":
+    exit(main())
