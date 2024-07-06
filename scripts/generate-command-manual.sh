77ac9575b9f96d4daab590674688c335ad461ccb
Ringo Hoffmann <riho@live.de>
2024-07-06 13:15:26 +0200

    update command manual

--- scripts/generate-command-manual.sh
+++ scripts/generate-command-manual.sh
@@ -2,25 +2,25 @@
 
 OUT_DIR="docs/commands.md"
 
-header=""
-mans=""
-
 for cmd in src/commands/*.rs; do
-    cmd=$(basename $cmd)
+    cmd=$(basename "$cmd")
     [ "$cmd" == "mod.rs" ] && continue
     cmd=${cmd%*.rs}
     cmd=${cmd/_/-}
     
+    echo "Running helpf for command $cmd ..."
     output=$(cargo run --quiet -- "$cmd" --help)
 
-    headers+="- [$cmd](#$cmd): \``echo "$output" | head -n1`\`\n"
+    headers+="- [$cmd](#$cmd): \`$(echo "$output" | head -n1)\`\n"
     mans+="### $cmd\n\n> $ goup help $cmd\n\n\`\`\`\n$output\n\`\`\`\n\n"
 done
 
-echo -e "# Available Commands\n\n" > $OUT_DIR
-echo -e "> $ goup help\n" >> $OUT_DIR
-echo -e "\`\`\`\n`cargo run --quiet -- help`\n\`\`\`\n\n" >> $OUT_DIR
-echo -e "## Index\n\n" >> $OUT_DIR
-echo -e "$headers" >> $OUT_DIR
-echo -e "## Details\n\n" >> $OUT_DIR
-echo -e "$mans" >> $OUT_DIR
+{
+    echo -e "# Available Commands\n\n"
+    echo -e "> $ goup help\n"
+    echo -e "\`\`\`\n$(cargo run --quiet -- help)\n\`\`\`\n\n"
+    echo -e "## Index\n\n"
+    echo -e "$headers"
+    echo -e "## Details\n\n"
+    echo -e "$mans"
+} > $OUT_DIR
